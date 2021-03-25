06f6a9894f96b4e2496276f8911b3aa9e39b2f83
Winston Robson <43570913+gumdropsteve@users.noreply.github.com>
2021-03-24 20:21:18 -0700

    Create where_are_you_going.py

---
+++ where_are_you_going.py
@@ -0,0 +1,175 @@
+# note: both lists in this file should be in the same order; list 1 is used for urls, list 2 is used for file names
+
+# set locations | format: city--state--country
+locations = ['Oakland--California--United-States',
+             'San-Diego--California--United-States',
+             'San-Francisco--California--United-States',
+             'California--United-States',
+             
+             'Bentonville--Arkansas--United-States',
+             'Bella-Vista--Arkansas--United-States',
+             'Little-Rock--Arkansas--United-States',
+             'Arkansas--United-States',
+             
+             'Austin--Texas--United-States',
+             'Dallas--Texas--United-States',
+             'Houston--Texas--United-States',
+             'Texas--United-States',
+             
+             'Las-Vegas--Nevada--United-States',
+             'Paradise--Nevada--United-States',
+             'Henderson--Nevada--United-States',
+             'Reno--Nevada--United-States',
+             'Nevada--United-States',
+             
+             'Anchorage--Alaska--United-States',
+             'North-Pole--Alaska--United-States',
+             'Alaska--United-States',
+             
+             'Budapest--Hungary',
+             'Hungary',
+             
+             'Tokyo--Japan',
+             'Japan',
+            
+             'Florida-Keys--Florida--United-States',
+             'Miami--Florida--United-States',
+             'Florida--United-States',
+             
+             'Phu-Quoc-Island--Phú-Quốc--Kien-Giang--Vietnam',
+             'Ho-Chi-Minh--Vietnam',
+             'Can-Tho--Vietnam',
+             'Vietnam',
+             
+             'London--United-Kingdom',
+             'City-of-London--London--United-Kingdom',
+             'Camden-Town--London--United-Kingdom',
+             'Canary-Wharf--London--United-Kingdom',
+             'United-Kingdom',
+             
+             'Riyadh-Province--Saudi-Arabia',
+             'Jeddah--Makkah-Province--Saudi-Arabia',
+             'Dammam--Eastern-Province--Saudi-Arabia',
+             'Saudi-Arabia',
+             
+             'Tbilisi--Georgia',
+             'Georgia--Europe',
+             
+             'United-States',
+             
+             'Sydney--New-South-Wales--Australia',
+             'Melbourne--Victoria--Australia',
+             'Australia',
+             
+             'Boise--Idaho--United-States',
+             "Coeur-d'Alene--Idaho--United-States",
+             'Idaho--United-States',
+             
+             'New-Orleans--Louisiana--United-States',
+             'Baton-Rouge--Louisiana--United-States',
+             'Shreveport--Louisiana--United-States',
+             'Louisiana--United-States',
+             
+             'Philadelphia--Pennsylvania--United-States',
+             'Pennsylvania--United-States',
+             
+             'St.-Thomas--U.S.-Virgin-Islands',
+             'Virgin-Islands',
+             
+             'Honolulu--HI--United-States',
+             
+             'Soul--South-Korea',
+             
+             'Seattle--Washington--United-States',
+             
+             'Barcelona',
+             'Palma',
+             'Murcia',
+            ]
+
+# set location aliases (file names, e.g. 'oakland' is for oakland.parquet)
+location_aliases = ['oakland',
+                    'san_diego',
+                    'san_francisco',
+                    'california',
+                    
+                    'bentonville',
+                    'bella_vista',
+                    'little_rock',
+                    'arkansas',
+                    
+                    'austin',
+                    'dallas',
+                    'houston',
+                    'texas',
+                    
+                    'las_vegas',
+                    'paradise',
+                    'henderson',
+                    'reno',
+                    'nevada',
+                    
+                    'anchorage',
+                    'north_pole',
+                    'alaska',
+                    
+                    'budapest',
+                    'hungary',
+                    
+                    'tokyo',
+                    'japan',
+                   
+                    'florida_keys',
+                    'miami',
+                    'florida',
+                   
+                    'pho_quoc_island',
+                    'ho_chi_minh',
+                    'can_tho',
+                    'vietnam',
+                    
+                    'london',
+                    'city_of_london',
+                    'camden_town',
+                    'canary_wharf',
+                    'united_kingdom',
+                    
+                    'riyadh',
+                    'jeddah',
+                    'dammam',
+                    'saudi_arabia',
+                    
+                    'tbilisi',
+                    'georgia_country',
+                    
+                    'united_states',
+                    
+                    'sydney',
+                    'melbourne',
+                    'australia',
+                    
+                    'boise',
+                    'coeur_dalene',
+                    'idaho',
+                    
+                    'new_orleans',
+                    'baton_rouge',
+                    'shreveport',
+                    'louisiana',
+                    
+                    'philadelphia',
+                    'pennsylvania',
+                    
+                    'st_thomas',
+                    'virgin_islands',
+                    
+                    'honolulu',
+                    
+                    'seoul',
+                    
+                    'seattle',
+                    
+                    'barcelona',
+                    'palma',
+                    'murcia',
+                   ]
